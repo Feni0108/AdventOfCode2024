7d8bacf27edf6ba8225e19ff54f1b3a015343ffe
Feni0108 <94fanni@gmail.com>
2024-12-01 23:02:14 +0100

    Created a method that gives the correct result for the probe

--- Challenges/src/day1/HistorianHysteria.java
+++ Challenges/src/day1/HistorianHysteria.java
@@ -1,24 +1,26 @@
 package day1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class HistorianHysteria {
     public static void main(String[] args) {
         //Probe input
-        List<Integer> leftNumbers = new ArrayList<>(Arrays.asList(3, 4, 2, 1, 3, 3));
-        List<Integer> rightNumbers = new ArrayList<>(Arrays.asList(4, 3, 5, 3, 9, 3));
+        int[] leftNumbers = new int[]{3, 4, 2, 1, 3, 3};
+        int[] rightNumbers = new int[]{4, 3, 5, 3, 9, 3};
+        Arrays.sort(leftNumbers);
+        Arrays.sort(rightNumbers);
+
+        getSumOfDistances(leftNumbers, rightNumbers);
     }
 
-    //Have to sort the to lists, and delete duplications
-    public static List<Integer> sortAndDropDuplications(List<Integer> numbers) {
-        Collections.sort(numbers);
-        List<Integer> sortedAndDistinct = numbers.stream()
-                .distinct()
-                .collect(Collectors.toList());
-        return sortedAndDistinct;
+    //Have to sort the arrays
+    //Have to pair up the numbers of the two list and calculate the distance between them
+    public static int getSumOfDistances(int[] leftNumbers, int[] rightNumbers) {
+        int sumOfDistances = 0;
+        for (int i = 0; i < leftNumbers.length; i++) {
+            sumOfDistances += Math.abs(leftNumbers[i] - rightNumbers[i]);
+            System.out.println(sumOfDistances);
+        }
+        return sumOfDistances;
     }
 }
