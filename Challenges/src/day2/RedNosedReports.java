6d574d3691d1bdb8249381eef749fdedbbf6cf20
Feni0108 <94fanni@gmail.com>
2024-12-04 23:21:59 +0100

    Day3 solution for the first puzzle

--- Challenges/src/day2/RedNosedReports.java
+++ Challenges/src/day2/RedNosedReports.java
@@ -6,16 +6,61 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*Brainstorming
+*
+* I have to delete every element from a list, and see if those two methods are true without those elements.
+* Sum up the elements without them the methods were false, and if it is one element, then it is a safe level.
+*
+* Always create a new list in  a loop not containing the specific element
+*
+* */
+
 public class RedNosedReports {
     public static void main(String[] args) {
         //Probe input
-        List<Integer> level = new ArrayList<Integer>(Arrays.asList(8, 10, 11, 14, 23));
+        List<Integer> level = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 4, 6, 7));
+        List<Integer> level2 = new ArrayList<Integer>(Arrays.asList(82, 84, 85, 87, 90, 92, 93, 91));
 
         //Input
         String inputTitle = "day2input.txt";
 
-        System.out.println(isIncreasing(level));
+        //First puzzle solution
+        System.out.println(isIncreasing(level2));
         System.out.println(sumOfSafeLevels(readByLine(inputTitle)));
+
+        //Second puzzle solution
+        //If only one level is problematic
+        System.out.println(secondPuzzleResult(readByLine(inputTitle)));
+    }
+
+    public static int secondPuzzleResult(List<List<Integer>> allLevels) {
+        int sum = 0;
+        for (int i = 0; i < allLevels.size(); i++) {
+            if (isDecreasing(allLevels.get(i)) || isIncreasing(allLevels.get(i))) {
+                sum++;
+            } else if(getFaultyLevels(allLevels.get(i))) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    public static boolean getFaultyLevels(List<Integer> level) {
+        int sumOfFaultyLevels = 0;
+        for (int i = 0; i < level.size(); i++) {
+            List<Integer> newLevels = new ArrayList<>(level);
+            newLevels.remove(i);
+
+            if(isIncreasing(newLevels) || isDecreasing(newLevels)) {
+                sumOfFaultyLevels++;
+            }
+        }
+
+        if (sumOfFaultyLevels >= 1) {
+            return true;
+        }
+        return false;
     }
 
     //If (isDecreasing || isDecreasing)
---
+++ Challenges/src/day3/MullItOver.java
@@ -0,0 +1,77 @@
+package day3;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class MullItOver {
+    public static void main(String[] args) {
+        String inputPath = "day3input.txt";
+        String testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(testInput);
+        while(matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+        //First puzzle
+        String wholeMemory = readToString(inputPath);
+        List<String> withoutCorruption = ignoreCorruptedMemory(wholeMemory);
+        List<Integer> multiplications = getMultiplications(withoutCorruption);
+        int sum = multiplications.stream()
+                        .reduce(0, Integer::sum);
+        System.out.println(sum);
+    }
+
+    //Parse string numbers and make multiplications
+    public static List<Integer> getMultiplications(List<String> withoutCorrupted) {
+        List<Integer> multiplications = new ArrayList<>();
+        for (String str : withoutCorrupted) {
+            String firstNumber = "";
+            String secondNumber = "";
+            boolean change = false;
+            for (char c : str.toCharArray()) {
+                if (c == ',') {
+                    change = true;
+                } else if (!change && Character.isDigit(c)) {
+                    firstNumber += c;
+                } else if (change && Character.isDigit(c)) {
+                    secondNumber += c;
+                }
+            }
+            multiplications.add(Integer.parseInt(firstNumber) * Integer.parseInt(secondNumber));
+        }
+        return multiplications;
+    }
+
+    //Get only the patterns in a list
+    public static List<String> ignoreCorruptedMemory(String memory) {
+        List<String> withoutCorruptedMemory = new ArrayList<>();
+        Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(memory);
+        while(matcher.find()) {
+            withoutCorruptedMemory.add(matcher.group());
+        }
+        return withoutCorruptedMemory;
+    }
+
+    //Read file to a string
+    public static String readToString(String path) {
+        String memory = "";
+        try {
+            FileReader reader = new FileReader(path);
+            int data = 0;
+            while ((data = reader.read()) != -1) {
+                memory += (char)data;
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        return memory;
+    }
+}
